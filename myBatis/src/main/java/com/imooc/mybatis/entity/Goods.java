40ae110bd47004a25cf781a16840ec2e9c1a6b25
ludaqing <ludaqing@ictnj.ac.cn>
2023-03-17 10:46:47 +0800

    创建实体类，单表查询多参数实践

---
+++ myBatis/src/main/java/com/imooc/mybatis/entity/Goods.java
@@ -0,0 +1,78 @@
+package com.imooc.mybatis.entity;
+
+public class Goods {
+    private Integer goodsId;
+    private String title;
+    private String subTitle;
+    private Float originalCost;
+    private Float currentPrice;
+    private Float discount;
+    private Integer isFreeDelivery;
+    private Integer categoryID;
+
+    public Integer getGoodsId() {
+        return goodsId;
+    }
+
+    public void setGoodsId(Integer goodsId) {
+        this.goodsId = goodsId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getSubTitle() {
+        return subTitle;
+    }
+
+    public void setSubTitle(String subTitle) {
+        this.subTitle = subTitle;
+    }
+
+    public Float getOriginalCost() {
+        return originalCost;
+    }
+
+    public void setOriginalCost(Float originalCost) {
+        this.originalCost = originalCost;
+    }
+
+    public Float getCurrentPrice() {
+        return currentPrice;
+    }
+
+    public void setCurrentPrice(Float currentPrice) {
+        this.currentPrice = currentPrice;
+    }
+
+    public Float getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(Float discount) {
+        this.discount = discount;
+    }
+
+    public Integer getIsFreeDelivery() {
+        return isFreeDelivery;
+    }
+
+    public void setIsFreeDelivery(Integer isFreeDelivery) {
+        this.isFreeDelivery = isFreeDelivery;
+    }
+
+    public Integer getCategoryID() {
+        return categoryID;
+    }
+
+    public void setCategoryID(Integer categoryID) {
+        this.categoryID = categoryID;
+    }
+
+
+}
