e11bf3f0704c746d2561c46acf6a2743b78e313b
ludaqing <ludaqing@ictnj.ac.cn>
2023-03-17 10:43:55 +0800

    mybatis单表单参数多参数查询 测试

--- myBatis/src/test/java/com/imooc/mybatis/MyBatisTestor.java
+++ myBatis/src/test/java/com/imooc/mybatis/MyBatisTestor.java
@@ -1,5 +1,6 @@
 package com.imooc.mybatis;
 
+import com.imooc.mybatis.entity.Goods;
 import com.imooc.mybatis.utils.MyBatisUtils;
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
@@ -10,6 +11,9 @@ import org.junit.Test;
 import java.io.IOException;
 import java.io.Reader;
 import java.sql.Connection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class MyBatisTestor {
     @Test
@@ -30,7 +34,7 @@ public class MyBatisTestor {
     }
 
     @Test
-    public void testMyBatisUtils(){
+    public void testMyBatisUtils() {
         SqlSession sqlSession = null;
         try {
             sqlSession = MyBatisUtils.openSession();
@@ -42,5 +46,55 @@ public class MyBatisTestor {
             MyBatisUtils.closeSession(sqlSession);
         }
     }
+
+    @Test
+    public void queryGoods() {
+        SqlSession sqlSession = null;
+        try {
+            sqlSession = MyBatisUtils.openSession();
+            List<Goods> list = sqlSession.selectList("goods.queryAll");
+            list.stream().forEach(good -> {
+                System.out.println(good.getTitle());
+            });
+            System.out.println(list);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        } finally {
+            MyBatisUtils.closeSession(sqlSession);
+        }
+    }
+
+    @Test
+    public void queryGoodsByID() {
+        SqlSession sqlSession = null;
+        try {
+            sqlSession = MyBatisUtils.openSession();
+            Goods goods = sqlSession.selectOne("goods.queryGoodsById", 1602);
+            System.out.println(goods.getTitle());
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        } finally {
+            MyBatisUtils.closeSession(sqlSession);
+        }
+    }
+
+    @Test
+    public void queryByPriceRange() {
+        SqlSession sqlSession = null;
+        try {
+            sqlSession = MyBatisUtils.openSession();
+            Map params = new HashMap<>();
+            params.put("min", 100);
+            params.put("max", 500);
+            params.put("limit", 10);
+            List<Goods> list = sqlSession.selectList("goods.queryByPriceRange", params);
+            list.stream().forEach(good-> System.out.println(good.getCurrentPrice()));
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        } finally {
+            MyBatisUtils.closeSession(sqlSession);
+        }
+    }
+
 
 }
