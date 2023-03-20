92398966afee01f7defe53af3cf926cd8a9a50e2
ludaqing <ludaqing@ictnj.ac.cn>
2023-03-20 17:09:56 +0800

    myBatis批量增加与删除的示例

--- myBatis/src/test/java/com/imooc/mybatis/MyBatisTestor.java
+++ myBatis/src/test/java/com/imooc/mybatis/MyBatisTestor.java
@@ -15,6 +15,7 @@ import org.junit.Test;
 import java.io.IOException;
 import java.io.Reader;
 import java.sql.Connection;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -227,9 +228,58 @@ public class MyBatisTestor {
         try {
             sqlSession = MyBatisUtils.openSession();
             PageHelper.startPage(1, 10);
-            List<Goods> list =  sqlSession.selectList("goods.queryByPage");
+            List<Goods> list = sqlSession.selectList("goods.queryByPage");
             PageInfo pageInfo = new PageInfo(list);
             System.out.println(pageInfo.toString());
+            sqlSession.commit();
+        } catch (Exception e) {
+            if (sqlSession != null) sqlSession.rollback();
+            throw new RuntimeException(e);
+        } finally {
+            MyBatisUtils.closeSession(sqlSession);
+        }
+    }
+
+    @Test
+    public void testBanchInsert() {
+        SqlSession sqlSession = null;
+        try {
+            sqlSession = MyBatisUtils.openSession();
+            List<Goods> list = new ArrayList<>();
+            for (int i = 0; i < 500; i++) {
+                Goods goods = new Goods();
+                goods.setTitle("测试商品" + i);
+                goods.setSubTitle("测试子标题");
+                goods.setOriginalCost(200f);
+                goods.setCurrentPrice(100f);
+                goods.setDiscount(0.5f);
+                goods.setIsFreeDelivery(1);
+                goods.setCategoryId(43);
+                list.add(goods);
+            }
+            int num = sqlSession.insert("goods.batchInsert", list);
+            System.out.println(num);
+            sqlSession.commit();
+        } catch (Exception e) {
+            if (sqlSession != null) sqlSession.rollback();
+            throw new RuntimeException(e);
+        } finally {
+            MyBatisUtils.closeSession(sqlSession);
+        }
+    }
+
+    @Test
+    public void testBanchDelete() {
+        SqlSession sqlSession = null;
+        try {
+            sqlSession = MyBatisUtils.openSession();
+            List<Integer> list = new ArrayList<>();
+            for (int i = 2674; i < 3180; i++) {
+                list.add(i);
+            }
+
+            int num = sqlSession.insert("goods.banchDelete", list);
+            System.out.println(num);
             sqlSession.commit();
         } catch (Exception e) {
             if (sqlSession != null) sqlSession.rollback();
